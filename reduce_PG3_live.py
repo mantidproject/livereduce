c64b679235bccd9ee6ac44319a32c61583c6ba50
Pete Peterson <petersonpf@ornl.gov>
2017-01-06 12:52:32 -0500

    Adding configuration file

--- reduce_PG3_live.py
+++ reduce_PG3_live.py
@@ -1,6 +1,8 @@
 from __future__ import (absolute_import, division, print_function)
+import json
 import os
 import signal
+import sys
 import time
 
 ####################
@@ -39,27 +41,82 @@ def shutdown_mantid():
         print('shutting down mantid')
         AlgorithmManager.cancelAll()
 
+class Config(object):
+    '''
+    Configuration storred in json format. The keys are:
+    * 'instrument' - missing generates an error
+    * 'mantid_loc' - if not specified, goes to environment variable
+      ${MANTIDPATH} then defaults to '/opt/Mantid/bin/'
+    * 'script_dir' - default value is '/SNS/{instrument}/shared/livereduce'
+    '''
+
+    def __init__(self, filename='/etc/liveprocessing.conf'):
+        '''Optional arguemnt is the json formatted config file'''
+        # read file from json into a dict
+        with open(filename, 'r') as handle:
+            json_doc = json.load(handle)
+
+        # get mantid location and add to the python path
+        self.mantid_loc = json_doc.get('mantid_loc')
+        if self.mantid_loc is None:
+            self.mantid_loc = os.environ.get('MANTIDPATH')
+            if self.mantid_loc is None:
+                self.mantid_loc = '/opt/Mantid/bin/'
+        sys.path.append(self.mantid_loc)
+
+        # instrument is critical
+        self.instrument = self.__getInstrument(json_doc.get('instrument'))
+
+        # location of the scripts
+        self.script_dir = json_doc.get('script_dir')
+        if self.script_dir is None:
+            self.script_dir = '/SNS/%s/shared/livereduce' % self.instrument.shortName()
+        self.script_dir = str(self.script_dir)
+
+        self.__determineScriptNames()
+
+    def __getInstrument(self, instrument):
+        from mantid import ConfigService
+        if instrument is None:
+            print('Using default instrument')
+            return ConfigService.getInstrument()
+        else:
+            return ConfigService.getInstrument(str(instrument))
+
+    def __determineScriptNames(self):
+        filenameStart = 'reduce_%s_live' % str(self.instrument.shortName())
+
+        # script for processing each chunk
+        self.procScript = filenameStart + '_proc.py'
+        self.procScript = os.path.join(self.script_dir, self.procScript)
+        if not os.path.exists(self.procScript):
+            raise RuntimeError('ProcessingScriptFilename \'%s\' does not exist' %
+                               self.procScript)
+
+        # script for processing accumulation
+        self.postProcScript = filenameStart + '_post_proc.py'
+        self.postProcScript = os.path.join(self.script_dir, self.postProcScript)
+        if not os.path.exists(self.postProcScript):
+            raise RuntimeError('PostProcessingScriptFilename \'%s\' does not exist' %
+                               self.postProcScript)
+
+    def toJson(self):
+        values = dict(instrument=self.instrument.shortName(),
+                      mantid_loc=self.mantid_loc,
+                      script_dir=self.script_dir)
+        return json.dumps(values)
+
+config = Config('liveprocessing.conf')
+print(config.toJson())
 
 from mantid import AlgorithmManager # required for clean shutdown to work
 from mantid.simpleapi import StartLiveData
 
-direc = '/SNS/PG3/shared/livereduce'
-direc = '/home/pf9/Dropbox/projects'
-procScript = os.path.join(direc, 'reduce_PG3_live_proc.py')
-postProcScript = os.path.join(direc, 'reduce_PG3_live_post_proc.py')
-
-if not os.path.exists(procScript):
-    raise RuntimeError('ProcessingScriptFilename \'%s\' does not exist' %
-                       procScript)
-if not os.path.exists(postProcScript):
-    raise RuntimeError('PostProcessingScriptFilename \'%s\' does not exist' %
-                       procScript)
-
 # need handle to the `MonitorLiveData` algorithm or it only runs once
 try:
-    StartLiveData(Instrument='POWGEN',
-                  ProcessingScriptFilename=procScript,
-                  PostProcessingScriptFilename=postProcScript,
+    StartLiveData(Instrument=config.instrument.name(),
+                  ProcessingScriptFilename=config.procScript,
+                  PostProcessingScriptFilename=config.postProcScript,
                   FromNow=False,
                   FromStartOfRun=True,
                   UpdateEvery=30,
