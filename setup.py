3428a918e6289590738947c56729801529af4e57
Pete Peterson <petersonpf@ornl.gov>
2017-08-10 15:45:57 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 setup(name="livereduce",
-      version="1.0",
+      version="1.1",
       description = "Need a description",
       author = "Pete Peterson",
       author_email = "petersonpf@ornl.gov",
